15137c6762b2c22c2eb7fc916e8706bb4c678346
Erick <erick.v.l.s@gmail.com>
2019-09-03 20:58:07 -0300

    Using Fetch to get Data from API

--- Web-Server-Using-Express/public/js/app.js
+++ Web-Server-Using-Express/public/js/app.js
@@ -1,5 +1,15 @@
-fetch('http://localhost:3000/cotacoes?ativo=PETR4.SAasdasd').then( (response) => {
-    response.json().then((data)=>{
-        console.log(data.error);
+const formulario = document.querySelector('form');
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const inputValue = document.querySelector('input').value
+
+    if(!inputValue){
+        alert('Input should be filled')
+        return
+    }
+    fetch(`http://localhost:3000/cotacoes?ativo=${inputValue}`).then((response) => {
+        response.json().then((data) => {
+            console.log(data);
+        })
     })
-} )
+});
